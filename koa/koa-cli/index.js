8949230b4e78c75a3fd391f3bc18514515229211
LeiWang <pvtevan@outlook.com>
2019-04-16 14:11:27 +0800

    koa基础内容

---
+++ koa/koa-cli/index.js
@@ -0,0 +1,16 @@
+const Koa = require('koa')
+const app = new Koa()
+
+// 对于任何请求，app将调用该异步函数处理请求：
+app.use(async (ctx, next) => {
+  await next()
+  // todo
+})
+
+app.use(async (ctx, next) => {
+  await next()
+  // todo
+})
+
+// 在端口8081监听:
+app.listen(1210)
