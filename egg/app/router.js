ca32ff885c9fdd04e92de8549eff7ce547fabd5b
LeiWang <pvtevan@outlook.com>
2019-04-16 18:20:33 +0800

    初始化项目

---
+++ egg/app/router.js
@@ -0,0 +1,9 @@
+
+/**
+ * @param {Egg.Application} app - egg application
+ */
+module.exports = app => {
+  const { router, controller } = app
+  router.get('/', controller.home.index)
+  router.resources('topics', '/api/v2/topics', controller.topics)
+}
---
+++ egg/config/config.default.js
@@ -0,0 +1,37 @@
+/* eslint valid-jsdoc: "off" */
+
+/**
+ * @param {Egg.EggAppInfo} appInfo app info
+ */
+module.exports = appInfo => {
+  /**
+   * built-in config
+   * @type {Egg.EggAppConfig}
+   **/
+  const config = {
+    errorHandler: {
+      match: '/api'
+    }
+  }
+
+  // use for cookie sign key, should change to your own and keep security
+  config.keys = appInfo.name + '_1555406097809_5160'
+
+  // add your middleware config here
+  config.middleware = [ 'errorHandler' ]
+
+  config.validate = {
+    convert: false,
+    validateRoot: false
+  }
+
+  // add your user config here
+  const userConfig = {
+    // myAppName: 'egg',
+  }
+
+  return {
+    ...config,
+    ...userConfig
+  }
+}
